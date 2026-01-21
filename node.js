6d7b5b379f8dd5faff2d258d6aa8f952d5f4c1ca
Anuj <your-github-anujchh287@gmail.com>
2026-01-21 17:04:54 +0530

    some os function with setTime out

--- node.js
+++ node.js
@@ -1,14 +1,14 @@
-// const {reverse,upper} =require("./export.js");
-// const os=require('os');
+const {reverse,upper} =require("./export.js");
+const os=require('os');
 const fs=require('fs');
-// setTimeout(()=>{
-// console.log(os.platform())
-// console.log(os.hostname())
-// console.log(os.cpus())
+setTimeout(()=>{
+console.log(os.platform())
+console.log(os.hostname())
+console.log(os.cpus())
 
-// },10000)
-// console.log(upper("hello guys"));
-// console.log(reverse("hello guys"));
+},10000)
+console.log(upper("hello guys"));
+console.log(reverse("hello guys"));
  fs.readFile("a.txt","utf-8",(err,data)=>{
     const anuj=data.length;
 
