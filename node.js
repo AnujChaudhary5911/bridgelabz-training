70e03238f7741dd9ae71328a72935254fc7f9954
Anuj <your-github-anujchh287@gmail.com>
2026-01-21 17:02:12 +0530

    now the data is overWrite to another not append

--- node.js
+++ node.js
@@ -1,10 +1,18 @@
-const {reverse,upper} =require("./export.js");
-const os=require('os');
+// const {reverse,upper} =require("./export.js");
+// const os=require('os');
 const fs=require('fs');
+// setTimeout(()=>{
+// console.log(os.platform())
+// console.log(os.hostname())
+// console.log(os.cpus())
 
+// },10000)
+// console.log(upper("hello guys"));
+// console.log(reverse("hello guys"));
  fs.readFile("a.txt","utf-8",(err,data)=>{
     const anuj=data.length;
 
-    fs.appendFileSync("b.txt",anuj.toString());
-    console.log("your work is done");
+   fs.writeFile("b.txt", anuj.toString(), (err) => {
+       
+    });
  })
